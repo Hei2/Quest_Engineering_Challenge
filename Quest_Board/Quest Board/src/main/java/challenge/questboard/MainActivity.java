2d7422290e295f52f34492f903f1cca8d514cc08
Hei2 <kjsalveson@hotmail.com>
2014-04-19 01:54:03 -0500

    Login Page Implemented
    Main Activity is now a login page. When the user enters the correct
    credentials, the Quest Board Activity starts (it's currently empty).
    Otherwise the user is warned about incorrect input. Currently it checks
    if the username field is empty, then the password field, else it
    determines if the two together match the required credentials.

--- Quest_Board/Quest Board/src/main/java/challenge/questboard/MainActivity.java
+++ Quest_Board/Quest Board/src/main/java/challenge/questboard/MainActivity.java
@@ -1,32 +1,33 @@
 package challenge.questboard;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.lang.reflect.Field;
 
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -36,5 +37,27 @@ public class MainActivity extends Activity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Retrieve the text in the username and password fields to verify the login attempt
+    public void login(View view) {
+        EditText usernameText = (EditText) findViewById(R.id.username_field);
+        EditText passwordText = (EditText) findViewById(R.id.password_field);
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (username.length() == 0) {
+            usernameText.setError("Username required.");
+        } else if (password.length() == 0) {
+            passwordText.setError("Password required.");
+        } else {
+            if (username.equals("Lancelot") && password.equals("arthurDoesntKnow")) {
+                Intent intent = new Intent(this, QuestBoardActivity.class);
+                startActivity(intent);
+            } else {
+                usernameText.setError("Username and/or Password incorrect.");
+                passwordText.setError("Username and/or Password incorrect.");
+            }
+        }
     }
 }
