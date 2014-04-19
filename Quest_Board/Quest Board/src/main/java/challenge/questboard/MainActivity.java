700106670fc94c4134117f1045a2780dd849bc5c
Hei2 <kjsalveson@hotmail.com>
2014-04-19 18:25:25 -0500

    Remember Username Implemented
    The username and checkbox state get saved to the app's preferences. A
    listener was added to the Main Activity's onCreate() to ensure the
    username is consistently updated in the preferences if the checkbox is
    checked.

--- Quest_Board/Quest Board/src/main/java/challenge/questboard/MainActivity.java
+++ Quest_Board/Quest Board/src/main/java/challenge/questboard/MainActivity.java
@@ -2,23 +2,62 @@ package challenge.questboard;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
-
-import java.lang.reflect.Field;
 
 // Display the login page to the user and handle the process of logging in
 public class MainActivity extends Activity {
+    // Username and password fields to be passed to the RegistrationActivity
     public final static String EXTRA_USERNAME = "challenge.questboard.USERNAME";
     public final static String EXTRA_PASSWORD = "challenge.questboard.PASSWORD";
+
+    // Username field and checkbox to be saved in the preferences
+    private static final String PREF_USERNAME = "username";
+    private static final String PREF_REMEMBER = "remember";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText usernameText = (EditText) findViewById(R.id.username_field);
+
+        // Determine if the "Remember Username" checkbox was checked, then fill in the username
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        if (pref.getBoolean(PREF_REMEMBER, false)) {
+            CheckBox rememberUsername = (CheckBox) findViewById(R.id.remember_username_checkbox);
+            rememberUsername.setChecked(true);
+            String username = pref.getString(PREF_USERNAME, "");
+            usernameText.setText(username);
+        }
+
+        // This listener is added to ensure the username gets saved
+        usernameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                // Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                // Do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (pref.getBoolean(PREF_REMEMBER, false)) {
+                    pref.edit().putString(PREF_USERNAME, usernameText.getText().toString()).commit();
+                }
+            }
+        });
     }
 
     @Override
@@ -34,8 +73,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
+        if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -73,5 +111,17 @@ public class MainActivity extends Activity {
         intent.putExtra(EXTRA_USERNAME, username);
         intent.putExtra(EXTRA_PASSWORD, password);
         startActivity(intent);
+    }
+
+    // Save the username and checkbox state to the preferences
+    public void remember(View view) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        CheckBox remember = (CheckBox) findViewById(R.id.remember_username_checkbox);
+        if (remember.isChecked()) {
+            EditText usernameText = (EditText) findViewById(R.id.username_field);
+            pref.edit().putString(PREF_USERNAME, usernameText.getText().toString()).putBoolean(PREF_REMEMBER, true).commit();
+        } else {
+            pref.edit().putBoolean(PREF_REMEMBER, false).commit();
+        }
     }
 }
